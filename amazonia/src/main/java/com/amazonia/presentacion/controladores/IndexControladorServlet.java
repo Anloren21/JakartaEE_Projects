314ee31c7bcd7014c7405de24b197feddbc2547a
Anloren21 <anlorerohu1996@gmail.com>
2026-09-13 21:51:57 +0200

    Controlador que nos puede llevar a index

---
+++ amazonia/src/main/java/com/amazonia/presentacion/controladores/IndexControladorServlet.java
@@ -0,0 +1,35 @@
+package com.amazonia.presentacion.controladores;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.amazonia.accesodato.ProductoCrud;
+import com.amazonia.dtos.Producto;
+
+@WebServlet("/index")
+public class IndexControladorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 1. Recibir la información de la petición
+		// 2. Convertir los datos necesarios
+		// 3. Crear objeto
+		// 4. Procesar datos
+		ArrayList<Producto> productos = ProductoCrud.obtenerTodos();
+		
+		System.out.println("Productos: " + productos);
+
+		// 5. Saltar a la siguiente pantalla/vista
+		request.setAttribute("productos", productos);
+
+		// 6. Saltar a la siguiente vista
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+}
