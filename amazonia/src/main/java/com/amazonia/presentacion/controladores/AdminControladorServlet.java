3b58ef261c912fb8ff00972002215001bb2c84a4
Anloren21 <anlorerohu1996@gmail.com>
2026-09-24 13:23:19 +0200

    Listado de productos por administración

---
+++ amazonia/src/main/java/com/amazonia/presentacion/controladores/AdminControladorServlet.java
@@ -0,0 +1,36 @@
+package com.amazonia.presentacion.controladores;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.amazonia.accesodato.ProductoCrud;
+import com.amazonia.dtos.Producto;
+import com.amazonia.logicanegocio.AnonimoNegocio;
+
+@WebServlet("/admin/listado")
+public class AdminControladorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 1. Recibir la información de la petición
+		// 2. Convertir los datos necesarios
+		// 3. Crear objeto
+		// 4. Procesar datos-Llamar a la lógica de negocio
+		ArrayList<Producto> productos = AnonimoNegocio.listarProductos();
+		
+		System.out.println("Productos: " + productos);
+
+		// 5. Saltar a la siguiente pantalla/vista
+		request.setAttribute("productos", productos);
+
+		// 6. Saltar a la siguiente vista
+		request.getRequestDispatcher("/admin/listado.jsp").forward(request, response);
+	}
+
+}
--- amazonia/src/main/webapp/includes/cabecera.jsp
+++ amazonia/src/main/webapp/includes/cabecera.jsp
@@ -28,11 +28,9 @@
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 					<li class="nav-item"><a class="nav-link active" href="index">Principal</a></li>
 				</ul>
-				<form class="d-flex" role="search">
-					<input class="form-control me-2" type="search" placeholder="Search"
-						aria-label="Search" />
-					<button class="btn btn-outline-success" type="submit">Search</button>
-				</form>
+				<ul class="navbar-nav mb-2 mb-lg-0">
+					<li class="nav-item"><a class="nav-link active" href="admin/listado">Administación</a></li>
+				</ul>
 			</div>
 		</div>
 	</nav>
